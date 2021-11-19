923881194f8a8fb39816d2a9c40581300a717b68
StepanSSA <slepcenkostepan403@gmail.com>
2021-11-19 14:56:24 +0500

    Update UsingAbilities

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -5,26 +5,4 @@ using UnityEngine;
 public class Hero : MonoBehaviour
 {
     
-    public string heroName;
-
-    public void SawAbility()
-    {
-        Debug.Log("Способность пилы");
-    }
-    public void TurtlesAbility()
-    {
-        Debug.Log("Способность черепахи");
-    }
-    public void HedgehogAbility()
-    {
-        Debug.Log("Способность ежа");
-    }
-    public void EelAbility()
-    {
-        Debug.Log("Способность угря");
-    }
-    public void HammerAbility()
-    {
-        Debug.Log("Способность молота");
-    }
 }
--- Assets/Scripts/UsingAbilities.cs
+++ Assets/Scripts/UsingAbilities.cs
@@ -4,25 +4,58 @@ using UnityEngine;
 
 public class UsingAbilities : MonoBehaviour
 {
-    private delegate void Methods();
-    private Dictionary<string, Methods> abilitiesDict;
     public HeroChanger heroChanger;
     public List<Hero> heroes;
-    void Start()
-    {
-        abilitiesDict = new Dictionary<string, Methods>();
-        var hero = new Hero();
-        abilitiesDict.Add("saw", new Methods(hero.SawAbility));
-        abilitiesDict.Add("turtle", new Methods(hero.TurtlesAbility));
-        abilitiesDict.Add("hammer", new Methods(hero.HammerAbility));
-        abilitiesDict.Add("hedgehog", new Methods(hero.HedgehogAbility));
-        abilitiesDict.Add("eel", new Methods(hero.EelAbility));
-    }
-
     public void UseAbilities()
     {
         var index = heroChanger.GetActiveHeroIndex;
-        var hero = heroes[index];
-        abilitiesDict[hero.heroName]();
+        switch (index)
+        {
+            case 0: HammerAbility(heroes[index]);
+                break;
+            case 1: SawAbility(heroes[index]);
+                break;
+            case 2: HedgehogAbility(heroes[index]);
+                break;
+            case 3: EelAbility(heroes[index]);
+                break;
+            case 4: TurtlesAbility(heroes[index]);
+                break;
+            
+        }
+        
+    }
+    private void SawAbility(Hero hero)
+    {
+        //Пилит дерево/врагов
+        //Спавнит облако пыли(анимация)
+        //В облаке удаляется препятствие
+        Debug.Log("Способность пилы");
+    }
+    private void TurtlesAbility(Hero hero)
+    {
+        //Прячется в панцирь
+        Debug.Log("Способность черепахи");
+    }
+    private void HedgehogAbility(Hero hero)
+    {
+        //Увеличивается или уменьшается с помощью анимации
+        if(hero.transform.localScale == new Vector3(1,1,1))
+            hero.transform.localScale = new Vector3(1.5f,1.5f,1.5f);
+        else
+            hero.transform.localScale = new Vector3(1,1,1);
+        Debug.Log("Способность ежа");
+    }
+    private void EelAbility(Hero hero)
+    {
+        //Проходит через провода
+        Debug.Log("Способность угря");
+    }
+    private void HammerAbility(Hero hero)
+    {
+        //Ломает головой камний/врагов
+        //Спавнит облако пыли(анимация)
+        //В облаке удаляется препятствие
+        Debug.Log("Способность молота");
     }
 }
